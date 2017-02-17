801edcc60d675e0d615e008d4aef5f29142f2d1b
Pavel Yakovlev <zhmactep@gmail.com>
2017-02-17 17:41:26 +0300

    Packages added

--- init.el
+++ init.el
@@ -1,47 +1,53 @@
-; Use both elpa and melpa repos
+;; Use both elpa and melpa repos
 (require 'package)
 (add-to-list
   'package-archives
   '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
+
+;; Comment all this after first run
 ;(package-refresh-contents)
+;(setq package-list '(ergoemacs-mode intero-mode all-the-icons neotree projectile smex))
+;(dolist (package package-list)
+;  (unless (package-installed-p package)
+;    (package-install package)))
 
-; Include package ergoemacs
-(require 'ergoemacs-mode)
-
-; Hide toolbar and scrollbar
+;; Hide toolbar and scrollbar
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-; Open in fullscreen
+;; Open in fullscreen
 (set-frame-parameter nil 'fullscreen 'fullboth)
 
-; Load Dracula theme
+;; Load Dracula theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dracula t)
 
-; Disable splash screen
+;; Disable splash screen
 (setq inhibit-startup-screen t)
 
-; Start ergoemacs mode
+;; Include package ergoemacs
+(require 'ergoemacs-mode)
+;; Start ergoemacs mode
 (setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
 (setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
 (ergoemacs-mode 1)
 
-; Use ido mode
+;; Use ido mode
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
 
-; Intero Haskell hook
+;; Intero Haskell hook
 (add-hook 'haskell-mode-hook 'intero-mode)
-; Add stack path to local PATH variable
+;; Add stack path to local PATH variable
 (add-to-list 'exec-path "/usr/local/bin/")
 (add-to-list 'exec-path "/Users/pavel/.local/bin/")
 
-; Include all-the-icons for pretty package tree
+;; Include all-the-icons for pretty package tree
 (require 'all-the-icons)
-; Include neotree for package tree
+;; Include neotree for package tree
 (require 'neotree)
 (require 'projectile)
 (defun neotree-project-root-dir-or-current-dir ()
@@ -63,5 +69,8 @@ current buffer directory."
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (setq projectile-switch-project-action 'neotree-projectile-action)
 
-; Set nice default font
+;; Set nice default font
 (set-default-font "Inconsolata 20")
+
+;; Set line numbers
+(global-linum-mode t)
